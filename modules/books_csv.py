621265634468c9a12865ba2a8d5e3aacdb33ed1d
GM <galstyanm03@gmail.com>
2025-12-07 19:33:23 +0400

    commit

--- modules/books_csv.py
+++ modules/books_csv.py
@@ -13,7 +13,7 @@ def load_books():
     try:
         df = pd.read_csv(url, encoding='utf-8-sig')
         df.columns = df.columns.str.strip()
-    
+        st.success(f"✅ Բեռնված է {len(df)} գիրք")
         return df
     except Exception as e:
         st.error(f"Error loading books: {e}")
